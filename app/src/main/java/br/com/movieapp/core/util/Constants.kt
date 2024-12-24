028f2059fbaf7cbee14dd9a6215c334a9c72cbdc
eliezerantonio <eliezerfernandoantonio@gmail.com>
2024-12-25 00:44:13 +0100

    fix 🐛(Interceptor): fix bug on connection with api

--- app/src/main/java/br/com/movieapp/core/util/Constants.kt
+++ app/src/main/java/br/com/movieapp/core/util/Constants.kt
@@ -2,9 +2,9 @@ package br.com.movieapp.core.util
 
 object Constants {
 
-    const val API_KEY_PARAM = ""
-    const val LANGUAGE_PARAM = ""
-    const val LANGUAGE_VALUE = ""
+    const val API_KEY_PARAM = "api_key"
+    const val LANGUAGE_PARAM = "language"
+    const val LANGUAGE_VALUE = "pt_PT"
 
 
 }
